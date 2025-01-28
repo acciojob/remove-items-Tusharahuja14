1c3f222ad9aaa4f4d30424bdfc75e68bdd2ec0f9
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-01-28 15:07:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let select=document.getElementById("colorSelect");
+
+function remove()
+{
+	let selectedindex=select.selectedIndex;
+	
+	if(selectedindex!==-1)
+	{
+		select.remove(selectedindex)
+	}
+}
